d3504455531c8ad847b7a46bc52ce2efeeb22614
Dev-Zisko <dev.oliverfrancisco@gmail.com>
2022-04-24 17:40:00 -0400

    Subida de Archivos

---
+++ app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+
+const app = express();
+
+// Middleware
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Rutas
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Middleware para Vue.js router modo history
+const history = require('connect-history-api-fallback');
+app.use(history());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', require('./routes/nota'));
+
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), () => {
+  console.log('Server listening on localhost:'+ app.get('port') + '...');
+});
+
+// Conexión base de datos
+const mongoose = require('mongoose');
+
+const uri = 'mongodb://localhost:27017/myapp';
+const options = {useNewUrlParser: true, useCreateIndex: true};
+
+// Or using promises
+mongoose.connect(uri, options).then(
+  /** ready to use. The `mongoose.connect()` promise resolves to mongoose instance. */
+  () => { console.log('Conectado a DB') },
+  /** handle initial connection error */
+  err => { console.log(err) }
+);
